667007295e847b37bec45a59cc4a5ed1a28a6620
Cameron Alexander <emptyflash@gmail.com>
2016-01-07 10:00:46 -0600

    Switch from cuda dnn for use on algorithmia

--- train_network.py
+++ train_network.py
@@ -18,7 +18,7 @@ import os
 import lasagne
 from lasagne.layers import InputLayer, DenseLayer, DropoutLayer
 from lasagne.layers import NonlinearityLayer
-from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+from lasagne.layers import Conv2DLayer as ConvLayer
 from lasagne.layers import MaxPool2DLayer as PoolLayer
 from lasagne.layers import LocalResponseNormalization2DLayer as NormLayer
 from lasagne.nonlinearities import sigmoid
